ad90f8ed7a28a7840f58ebab2b782c517b71c970
Tom Porter <tom.port@yahoo.co.uk>
2023-02-24 15:59:06 +0000

    added Dave's requester and responder correlation code as comments for future reference

--- thePublisher.js
+++ thePublisher.js
@@ -43,3 +43,70 @@ ActiveMq.getConnection().then(
       //  }, timeout);
     }
 ).catch( e => console.log(e) );
+
+/*
+
+REQUESTER
+
+ActiveMq = require("./ActiveMqConnection.js");
+
+// Send one request
+//
+// node requester queueName "request text" priority
+
+let reqQueueName = process.argv[2];
+if (!reqQueueName || reqQueueName.length == 0) {
+    reqQueueName = "defaultReq";
+}
+const reqQueueSpec = "/queue/" + reqQueueName;
+const respQueueSpec = reqQueueSpec + "Resp";
+
+
+let requestText = process.argv[3];
+if (!requestText || requestText.length == 0) {
+    requestText = "book room 101";
+}
+
+let priorityText = process.argv[4];
+if (!priorityText || priorityText.length == 0) {
+    priorityText = "3";
+}
+
+const requestPriority = parseInt(priorityText);
+
+const header = {
+    "priority": requestPriority,
+    "reply-to": respQueueSpec,
+    "persistent" : true
+};
+
+ActiveMq.getConnection().then(
+    (mqConnection) => {
+        let requestList = [];
+        mqConnection.subscribe(respQueueSpec, 
+            (data, headers) => {
+                  console.log('Response', data, headers);
+                  let correlationId = headers["correlation-id"];
+                  if ( correlationId && requestList[correlationId] ) {
+                    console.log('correlates ' + requestList[correlationId]);
+                  } else {
+                      console.log("no correlation.")
+                  }
+                }
+        );
+
+        for ( let i = 0; i < 2; i++){
+            header["correlation-id"] = i;
+            requestList[i] = "request " + i + ": " + requestText;
+            mqConnection.publish(reqQueueSpec, requestText + "," + i, header);
+        } 
+        
+        const timeout = 1000 * 60; // 1000 * 60 * 3 
+        setTimeout(() => {
+            mqConnection.disconnect(
+                () => console.log('DISCONNECTED')
+            );
+        }, timeout);
+    }
+).catch(e => console.log(e));
+*/
--- theSubscriber.js
+++ theSubscriber.js
@@ -48,3 +48,42 @@ ActiveMq.getConnection().then(
 ).catch( e => console.log(e) );
 
 
+/*
+
+RESPONDER
+
+ActiveMq = require("./ActiveMqConnection.js");
+
+// Responder, with example selector
+
+let reqQueueName = process.argv[2];
+if (!reqQueueName || reqQueueName.length == 0) {
+    reqQueueName = "defaultReq";
+}
+const reqQueueSpec = "/queue/" + reqQueueName;
+
+const subscribeHeader = {
+    selector: "JMSPriority < 6"
+};
+
+ActiveMq.getConnection().then(
+    ( mqConnection  ) => {
+        mqConnection.subscribe(reqQueueSpec, subscribeHeader, (data, headers) => {
+           console.log('Request\n', data, headers);
+           if ( headers["reply-to"] && headers["reply-to"].length > 0 ){
+               // persistent response for persistent request
+               let respHeader = { 
+                   "persistent": headers["persistent"], 
+                   "correlation-id": headers["correlation-id"], 
+                };
+
+               // reply to specfified queue
+               mqConnection.publish(headers["reply-to"], 'a response to ' + data, respHeader);
+           } else {
+               console.log("No response destination specified");
+           }
+        }); 
+        console.log("Booking service listening on " + reqQueueSpec); 
+    }
+).catch( e => console.log(e) );
+*/
